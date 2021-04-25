0719d3c8a782d4bfafbed833c71057e2fe47af62
TaillomParise <82917514+TaillomParise@users.noreply.github.com>
2021-04-25 17:26:23 -0300

    Add files via upload

---
+++ src/repositories/SettingsRepository.ts
@@ -0,0 +1,8 @@
+
+import { Entity, EntityRepository, Repository } from "typeorm";
+import { Setting } from "../entities/settings";
+
+@EntityRepository(Setting)
+class SettingsRepository extends Repository<Setting> {}
+
+export { SettingsRepository }
